3a666b03634b9c011fa73f9d33be25b6d7dd0cd1
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-22 21:16:26 +0300

    Feature: add user controllers
    Add getCurrentUser and updateUserInfo controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,1 +1,40 @@
+const User = require('../models/user');
+const NotFoundError = require('../errors/notFoundError');
+const BadRequestError = require('../errors/badRequestError');
 
+function getCurrentUser(req, res, next) {
+  const userId = req.user._id;
+
+  return User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError(`Пользователь по указанному _id: ${userId} не найден`);
+      }
+      res.send(user);
+    })
+    .catch(next);
+}
+
+function updateUserInfo(req, res, next) {
+  const { name, email } = req.body;
+
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError(`Пользователь с указанным _id: ${req.user._id} не найден.`);
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || 'CastError') {
+        return next(new BadRequestError('Переданы некорректные данные при обновлении профиля.'));
+      }
+      return next(err);
+    });
+}
+
+module.exports = { getCurrentUser, updateUserInfo };
